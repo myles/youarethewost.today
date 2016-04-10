3a739570687e32661a5edd421279db27628abc8b
Myles Braithwaite <me@mylesbraithwaite.com>
2016-04-09 22:55:50 -0400

    Upgrading some stuff.

--- tasks/_aliases.js
+++ tasks/_aliases.js
@@ -1,26 +1,25 @@
 module.exports = function (grunt) {
-	grunt.registerTask('run', [
-		'jekyll:serve'
-	]);
+  grunt.registerTask('run', [
+    'jekyll:serve'
+  ]);
 
-	grunt.registerTask('build', [
-		'jekyll:build'
-	]);
+  grunt.registerTask('build', [
+    'jekyll:build'
+  ]);
 
-	grunt.registerTask('deploy', [
-		// 'gitcommit:deploy',
-		'clean',
-		'jekyll:production',
-		's3:build',
-		'rsync:prod',
-		// 'slack'
-	]);
+  grunt.registerTask('deploy', [
+    'clean',
+    'jekyll:production',
+    'rsync:prod',
+    's3:build'
+  ]);
 
-	grunt.registerTask('staging', [
-		'jekyll:staging'
-	]);
+  grunt.registerTask('staging', [
+    'jekyll:staging'
+  ]);
 
-	grunt.registerTask('default', [
-		'run'
-	]);
+  grunt.registerTask('default', [
+    'run'
+  ]);
 };
+
